24a4b93194584b294d1fc6c1c957a455ab832e69
kaweesi <red@gmail>
2020-12-04 17:11:09 +0300

    Covid form by Kaweesi matia

--- kaweesimatia/public/js/farmerones.js
+++
@@ -1,4 +0,0 @@
-var x = document.getElementById("count");   // Get the element with id="demo"
-const farmerCount = require('../../models/agricOfficerModel')
-x = farmerCount.countDocuments()
-console.log(x)
